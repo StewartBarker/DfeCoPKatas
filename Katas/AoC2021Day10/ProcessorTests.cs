5853ba11621fac3f343676dd1cea9072fe80ae22
StewartBarker <50987431+StewartBarker@users.noreply.github.com>
2022-01-16 09:41:38 +0000

    Add initial test

--- Katas/AoC2021Day10/ProcessorTests.cs
+++ Katas/AoC2021Day10/ProcessorTests.cs
@@ -5,7 +5,15 @@ namespace AoC2021Day10;
 public class ProcessorTests
 {
     [Fact]
-    public void Test1()
+    public void GivenTestInputData_ResultShouldBeCorrect()
     {
+        // Arrange
+        var testInputData = System.IO.File.ReadAllLines("InputData/TestInput.txt");
+        
+        // Act
+        var result = Day10Processor.Execute(testInputData);
+
+        // Assert
+        Assert.Equal(26397, result);
     }
 }
