8fc61c0a6a1dfa8cdd4206147be0a3cbca2a55b0
StewartBarker <50987431+StewartBarker@users.noreply.github.com>
2022-01-16 10:44:02 +0000

    Update execute method, test passes with test input data

--- Katas/AoC2021Day10/Processor.cs
+++ Katas/AoC2021Day10/Processor.cs
@@ -1,12 +1,62 @@
 using System;
-using Xunit.Sdk;
+using System.Collections;
 
 namespace AoC2021Day10;
 
 public static class Processor
 {
+    
     public static int Execute(string[] data)
     {
-        return 0;
+        var characterStack = new Stack();
+        var score = 0;
+        foreach (var line in data)
+        {
+            foreach (var character in line)
+            {
+                if (IsOpeningParenthesis(character))
+                {
+                    characterStack.Push(character);
+                }
+                else
+                {
+                    var expected = GetMatchingOpeningParenthesis(character);
+                    var actual = (char)characterStack.Pop();
+                    if (actual != expected)
+                    {
+                        score += GetCharacterScore(expected);
+                        break;
+                    }
+                }
+            }
+        }
+        
+        bool IsOpeningParenthesis(char c) => ((IList)new[] { '{', '[', '(', '<' }).Contains(c);
+        
+        return score;
+    }
+
+    private static int GetCharacterScore(char openingParenthesis)
+    {
+        return openingParenthesis switch
+        {
+            '(' => 3,
+            '[' => 57,
+            '{' => 1197,
+            '<' => 25137,
+            _ => throw new ArgumentOutOfRangeException(nameof(openingParenthesis), $"{openingParenthesis} is an invalid character")
+        };
+    }
+
+    private static char GetMatchingOpeningParenthesis(char closingParenthesis)
+    {
+        return closingParenthesis switch
+        {
+            ']' => '[',
+            '}' => '{',
+            ')' => '(',
+            '>' => '<',
+            _ => throw new ArgumentOutOfRangeException(nameof(closingParenthesis), $"Can't find matching opening parenthesis for '{closingParenthesis}'")
+        };
     }
 }
